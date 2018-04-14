32fd610f79fb41414d6d1551b58ac209f811e508
Mmakwe Onyeka <notmaks@yahoo.com>
2018-04-14 22:40:37 +0100

    Added js for css gradient

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const result = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.getElementById("gradient");
+
+function colorpicker(){
+    body.style.background =
+    "linear-gradient(to right, "
+    + color1.value
+    + ", "
+    + color2.value
+    + ")";
+}
+color1.addEventListener("input", colorpicker)
+color2.addEventListener("input", colorpicker)
